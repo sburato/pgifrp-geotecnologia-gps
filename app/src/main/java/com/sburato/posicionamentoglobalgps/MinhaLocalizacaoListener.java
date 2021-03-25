7af790655ca9cdd237b063998d7766f882f8fbed
Silvano Burato <silvanoburato@gmail.com>
2021-03-24 23:22:41 -0300

    Adicionando Projeto

---
+++ app/src/main/java/com/sburato/posicionamentoglobalgps/MinhaLocalizacaoListener.java
@@ -0,0 +1,35 @@
+package com.sburato.posicionamentoglobalgps;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+public class MinhaLocalizacaoListener implements LocationListener {
+    public static double latitude;
+    public static double longitude;
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        location.getLatitude();
+        location.getLongitude();
+        this.latitude  = location.getLatitude();
+        this.longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        //
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        //
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        //
+    }
+}
